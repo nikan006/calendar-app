72896e3fdc4ba7d77d8b9364a48b17841df4120e
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-11-08 15:08:56 +0900

    userテストコードの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    it "nameが空では登録できない" do
-      
-    end
-    it "emailが空では登録できない" do
-      
-    end
-    it "passwordが空では登録できない" do
-      
-    end
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
+  describe "ユーザー新規登録" do
+    context "新規登録できる場合" do
+      it "全ての項目が入力されていれば実装できる" do
+        expect(@user).to be_valid
+      end
+    end
+    context "新規登録できない場合" do
+      it "nameが空では登録できない" do
+        @user.name = " "
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nameを入力してください"
+      end
+      it "emailが空では登録できない" do
+        @user.email = " "
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Emailを入力してください"
+      end
+      it "passwordが空では登録できない" do
+        @user.password = " "
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Passwordを入力してください"
+      end
+    end
   end
 end
