8e4706fe72254b2063fc38cc0b8ae40844f5a11b
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-11-13 15:27:54 +0900

    コメントボタンの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,10 @@ class CommentsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, event_id: params[:event_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "events/index"
   root to: "events#index"
   resources :events do
-    resources:comments, only: :create
+    resources:comments, only: [:create,:show]
   end
   resources :users, only: :show
 end
