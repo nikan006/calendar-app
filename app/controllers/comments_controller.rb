6f977eb33673c2be57c19dd1e9116cd4c9912a13
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-12-03 14:34:21 +0900

    コメントをDBに反映

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,22 @@
 class CommentsController < ApplicationController
+  
+  def new
+    @comment = Comment.new
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to root_path
+    @comment = Comment.create(comment_params)
+    redirect_to event_comment_path
   end
 
   def show
     @event = Event.find(params[:id])
+    @comment = Comment.find(params[:event_id])
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, event_id: params[:event_id])
+    params.require(:comment).permit(:text).merge(event_id: params[:event_id])
   end
 
 end
